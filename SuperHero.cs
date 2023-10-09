cab2bd1dc1e488663655fd9934bf6fa4789c3e33
Sandeep <sandeepchopra7@gmail.com>
2023-10-09 19:51:49 -0400

    add SuperHero model

---
+++ SuperHero.cs
@@ -0,0 +1,11 @@
+﻿namespace superhero;
+
+public class SuperHero
+{
+  public int Id { get; set; }
+  public string? Name { get; set; }
+  public string? FirstName { get; set; }
+  public string? LastName { get; set; }
+
+  public string? PlaceOfBirth { get; set; }
+}
