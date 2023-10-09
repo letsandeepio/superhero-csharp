8a7d43243772e486391ab67d450802aec2b1fc50
Sandeep <sandeepchopra7@gmail.com>
2023-10-09 19:52:00 -0400

    add SuperHero controller

---
+++ Controllers/SuperHeroController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace superhero.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class SuperHeroController : ControllerBase
+  {
+
+    private static List<SuperHero> heroes = new List<SuperHero>
+      {
+        new SuperHero { Id = 1, Name = "Spiderman 2", FirstName = "Peter", LastName = "Parker", PlaceOfBirth = "New York" },
+        new SuperHero { Id = 2, Name = "Batman 2", FirstName = "Bruce", LastName = "Wayne", PlaceOfBirth = "Gotham" },
+      };
+
+    [HttpGet]
+    public async Task<ActionResult<List<SuperHero>>> Get()
+    {
+      return Ok(heroes);
+    }
+  }
+}
