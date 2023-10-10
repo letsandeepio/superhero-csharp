ec6f602f00d0d5288fba52e96b6434c07d96a34b
Sandeep <sandeepchopra7@gmail.com>
2023-10-09 21:36:05 -0400

    add migrations

---
+++ Data/DataContext.cs
@@ -0,0 +1,14 @@
+
+using Microsoft.EntityFrameworkCore;
+
+namespace superhero.Data
+{
+  public class DataContext : DbContext
+  {
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+    public DbSet<SuperHero> SuperHeroes { get; set; }
+  }
+}
